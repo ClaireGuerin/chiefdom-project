188325567b6e22e1de1a73536078e60b3030e392
cguerin <guerin.claire01@gmail.com>
2019-06-17 16:55:53 +0200

    fertility function, correlation and mutation functions

--- lifecycle.py
+++ lifecycle.py
@@ -11,30 +11,35 @@ import numpy as np
 from functools import reduce
 
 def fertilityFunction(xi,x0,e,params):
-    gamma=params[0]
-    fb=params[1]
-    c=params[2]
-    b=params[3]
+    gamma = params[0]
+    fb = params[1]
+    c = params[2]
+    b = params[3]
     
     return max(0,round(fb*(1-c*xi[0]**2+b*x0[0])/(1+gamma*e[0]),3))
 
 def correlation(mutationStep,correlationCoefficient):
-    if len(mutationStep)>2:
-        sigmaColumn=np.repeat([mutationStep],len(mutationStep),axis=0)
-        sigmaRow=np.repeat(np.transpose([mutationStep]),len(mutationStep),axis=1)
-        rhoMatrix=np.repeat([np.repeat(float(correlationCoefficient),len(mutationStep))],len(mutationStep),axis=0)
+    numberTraits = len(mutationStep)
+    if numberTraits>1:
+        sigmaColumn = np.repeat([mutationStep],len(mutationStep),axis=0)
+        sigmaRow = np.repeat(np.transpose([mutationStep]),len(mutationStep),axis=1)
+        rhoMatrix = np.repeat([np.repeat(float(correlationCoefficient),len(mutationStep))],len(mutationStep),axis=0)
         np.fill_diagonal(rhoMatrix,1)
         
-        corr=reduce(np.multiply,[sigmaColumn, sigmaRow, rhoMatrix])
-    
-    elif len(mutationStep)>1:
-        
-        corr=correlationCoefficient
+        corr = reduce(np.multiply,[sigmaColumn, sigmaRow, rhoMatrix])
     
     else:
-        corr=np.nan
+        corr = np.nan
         
     return corr
     
+def mutation(mutationStep,correlationPattern,numberMutants):
+    numberTraits = len(mutationStep)
+    if numberTraits>1:
+        mut = np.random.multivariate_normal(np.repeat(0,numberTraits),correlationPattern,numberMutants)
     
+    else:
+        mut = np.random.normal(0,mutationStep,numberMutants)
+        
+    return mut
         
