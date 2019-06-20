e87638e6a2df13e061ed594f2a74cf9deb796414
cguerin <guerin.claire01@gmail.com>
2019-06-20 19:27:49 +0200

    some pb to solve with recursion over generations

--- runsim.py
+++ runsim.py
@@ -8,19 +8,36 @@ Created on Thu Jun 20 12:44:07 2019
 import numpy as np
 import lifecycle as lc
                  
-# Initial conditions and parameters     
-phen = [np.array([[0.4],[0.5]]),np.array([[0.1],[0.2],[0.3]]),np.array([[0.6],[0.7],[0.8],[0.9]])]
-env = []
-for i in phen: env.append([len(i)])
-                
-parsDict = {'probability mutation':0.5, 
+# Initial conditions and parameters  
+globalParsDict = {'demes number':3,
+                  'generations number':10,
+                  'initial phenotypes':[0.1],
+                  'initial deme size':1} 
+
+localParsDict = {'probability mutation':0.5, 
             'mutation step':[0.2], 
             'density competition':0.1, 
             'basal fertility':2, 
             'cooperation cost':0.05, 
             'cooperation benefit':0.5, 
             'dispersal rate':0.5, 
-            'demes number':len(phen), 
-            'mutation correlation coefficient':0}    
+            'demes number':globalParsDict['demes number'], 
+            'mutation correlation coefficient':0} 
+  
 
-lc.lifeCycle([phen,env],parsDict)
+def populationDynamics(globalParameters,localParameters):
+    initDemeSize = globalParameters['initial deme size']
+    generationsNumber = globalParameters['generations number']
+    phenotypes = globalParameters['initial phenotypes']
+    demesNumber = globalParameters['demes number']
+    
+    initialPhenotypes = [np.repeat([phenotypes],initDemeSize,axis=0)] * demesNumber
+    initialEnvironment = []
+    for i in initialPhenotypes: initialEnvironment.append([len(i)])
+    tmpPop = [initialPhenotypes,initialEnvironment]
+    
+    for gen in range(generationsNumber):
+        newTmpPop = lc.lifeCycle(tmpPop,localParameters)
+        tmpPop = newTmpPop
+        
+populationDynamics(globalParsDict,localParsDict)
